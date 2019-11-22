4bd5e64a4dcdcbe716d4002e248515f6096302f4
BakhtawarMaqsood <49411659+BakhtawarMaqsood@users.noreply.github.com>
2019-11-23 02:07:32 +0500

    retrieving Collection

--- EmailSearching/app/src/main/java/uet/se/emailsearching/MainActivity.java
+++ EmailSearching/app/src/main/java/uet/se/emailsearching/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,6 +15,8 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
@@ -22,10 +25,18 @@ public class MainActivity extends AppCompatActivity {
     EditText pass;
     TextView tosignup;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+    int i = 0;
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                .setTimestampsInSnapshotsEnabled(true)
+                .build();
+        db.setFirestoreSettings(settings);
+        final String[] currentCollection={""};
+
         login=findViewById(R.id.login);
         email=findViewById(R.id.email_login);
         pass=findViewById(R.id.password_login);
@@ -48,7 +59,14 @@ public class MainActivity extends AppCompatActivity {
                 db.collection(firstCh.toString()).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
                     @Override
                     public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-                        
+                        for(QueryDocumentSnapshot documentSnapshot: queryDocumentSnapshots)
+                        {
+                            SignUpData data = documentSnapshot.toObject(SignUpData.class);
+                            String index = String.valueOf(i);
+                            Log.d(TAG,index);
+                            Log.d(TAG, data.getEmail());
+                            i=i+1;
+                        }
                     }
                 }).addOnFailureListener(new OnFailureListener() {
                     @Override
