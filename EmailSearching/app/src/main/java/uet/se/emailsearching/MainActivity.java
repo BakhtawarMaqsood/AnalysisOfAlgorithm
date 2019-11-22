58f0df4b3cde7ef463fed3ac0aa93b37b7fb24d7
BakhtawarMaqsood <49411659+BakhtawarMaqsood@users.noreply.github.com>
2019-11-22 14:17:57 +0500

    Update MainActivity.java
    login_initialization

--- EmailSearching/app/src/main/java/uet/se/emailsearching/MainActivity.java
+++ EmailSearching/app/src/main/java/uet/se/emailsearching/MainActivity.java
@@ -1,5 +1,6 @@
 package uet.se.emailsearching;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,8 +9,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
     Button login;
@@ -31,6 +36,26 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, SignUp.class));
+            }
+        });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String currentEmail = email.getText().toString().trim();
+                String currentPassword = pass.getText().toString().trim();
+                Character firstCh = currentEmail.charAt(0);
+                db.collection(firstCh.toString()).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
     }
