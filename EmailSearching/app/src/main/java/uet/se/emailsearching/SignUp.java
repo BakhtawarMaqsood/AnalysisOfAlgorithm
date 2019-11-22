b4ca57dff6cbb030d36aaa203adb376336d00e36
BakhtawarMaqsood <49411659+BakhtawarMaqsood@users.noreply.github.com>
2019-11-22 12:30:16 +0500

    getting email

--- EmailSearching/app/src/main/java/uet/se/emailsearching/SignUp.java
+++ EmailSearching/app/src/main/java/uet/se/emailsearching/SignUp.java
@@ -9,18 +9,24 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class SignUp extends AppCompatActivity {
-    Button bt;
+    Button signup;
     EditText email;
     EditText password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-        email=findViewById(R.id.email_id);
-        password=findViewById(R.id.password_id);
-        bt.setOnClickListener(new View.OnClickListener() {
+        email=findViewById(R.id.email_signup);
+        password=findViewById(R.id.password_signup);
+        signup = (Button) findViewById(R.id.signup);
+
+        signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                String currentEmail = email.getText().toString().trim();
+                Character firstCh = currentEmail.charAt(0);
+                
                 Intent intent=new Intent(SignUp.this,MainActivity.class);
                 startActivity(intent);
             }
